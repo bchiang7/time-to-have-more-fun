ef74d8210f7456c1670cc07fa2f8fe96f2aed5ea
Brittany Chiang <brittany.chiang@gmail.com>
2020-01-02 23:41:59 -0500

    Add favicon

--- components/head.js
+++ components/head.js
@@ -12,10 +12,12 @@ const Head = props => (
     <title>{props.title || ''}</title>
     <meta name="description" content={props.description || defaultDescription} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/public/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/public/touch-icon.png" />
+    <link rel="icon" href="/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
     <link rel="mask-icon" href="/public/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/public/favicon.ico" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta property="og:description" content={props.description || defaultDescription} />
